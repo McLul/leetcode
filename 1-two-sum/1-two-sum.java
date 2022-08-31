23ba8cc1af710f1997aabd5eefadd17675347fc5
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2022-08-31 16:32:32 -0700

    Time: 90 ms (27.02%), Space: 44.8 MB (69.95%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,16 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int index = 0;
-        int tempTarget = target - nums[index];
+        target -= nums[index];
         while (index < nums.length-1) {
           for (int i = index+1; i < nums.length; i++) {
-                if (tempTarget - nums[i] == 0)
+                if (target - nums[i] == 0)
                 {
                     return new int[] {index, i};
                 }
             }
-            index += 1;
-            tempTarget = target - nums[index];
+            target += nums[index++] - nums[index];
         }
         return new int[] {};
     }
