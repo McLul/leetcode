de9a72fa953b44b05e35e89aaa92d5bb98b7f3dd
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2022-09-01 16:43:05 -0700

    Time: 0 ms (100.00%), Space: 42.9 MB (94.81%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int index = (int)Math.ceil(nums.length/2);
+        int lowerBound = 0;
+        int upperBound = nums.length-1;
+        while (nums[index] != target && lowerBound != upperBound) {
+            if (nums[index] < target) {
+                lowerBound = index+1;
+            } else if (nums[index] > target) {
+                upperBound = index-1;
+            } 
+            index = (int)Math.ceil(((upperBound - lowerBound)/2) + lowerBound);
+        }
+        if (nums[index] == target) {
+            return index;
+        } else {
+            return -1;
+        }
+    }
+}
