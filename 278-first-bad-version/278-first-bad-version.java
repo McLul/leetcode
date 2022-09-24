41c32c82c8158a55b45eac43aad71e19d7793b5c
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2022-09-24 16:45:48 -0700

    Time: 45 ms (6.25%), Space: 41.7 MB (7.01%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.java
@@ -0,0 +1,32 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        if (n == 1) {
+            if (isBadVersion(n)) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else if (n == (int)Math.pow(2,31)) {
+            if (isBadVersion((int)Math.pow(2,31) - 1)){
+                n = (int)Math.pow(2,31)-1;
+            } else {
+                return n;
+            }
+        }
+        int min = 1;
+        int max  = n;
+        int index = (int)Math.floor(n/2);
+        while (isBadVersion(index-1) != false || isBadVersion(index+1) != true || isBadVersion(index) != true) {
+            if (isBadVersion(index)) {
+                max = index-1;
+            } else {
+                min = index+1;
+            }
+            index = (int)Math.floor((max-min)/2 + min);
+        }
+        return index;
+     }
+}
