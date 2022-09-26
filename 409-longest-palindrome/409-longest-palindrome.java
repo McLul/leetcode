c2f7031e7f351111f54536f7024c249feef8f8eb
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2022-09-26 13:07:43 -0700

    Time: 9 ms (39.87%), Space: 42.9 MB (16.71%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.java
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -1,7 +1,6 @@
 class Solution {
     public int longestPalindrome(String s) {
         int results = 0;
-        boolean singleChar = false;
         HashMap<Character, Integer> mappedString = new HashMap<Character, Integer>();
         for (int i = 0; i < s.length(); i++) {
             char myChar = s.charAt(i);
@@ -13,11 +12,8 @@ class Solution {
         }
         for (char i : mappedString.keySet()) {
             results += Math.floor(mappedString.get(i)/2)*2;
-            if (mappedString.get(i)%2 != 0) {
-                singleChar = true;
-            }
         }
-        if (singleChar) {
+        if (s.length()-results > 0) {
             return results + 1;
         } else {
             return results;
