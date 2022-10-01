c7d3a0c86714ff1f61b241bfdd599d6342cfbd69
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2022-09-30 19:34:53 -0700

    Time: 13 ms (36.80%), Space: 47.5 MB (87.72%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -4,8 +4,9 @@ class Solution {
             return nums[0];
         }
         HashMap<Integer,Integer> mappedNums = new HashMap<Integer,Integer>();
+        int num = 0;
         for (int i = 0; i <nums.length; i++) {
-            int num = nums[i];
+            num = nums[i];
             if (mappedNums.get(num) == null) {
                 mappedNums.put(num, 1);
             } else if (mappedNums.get(num) + 1 > nums.length/2) {
