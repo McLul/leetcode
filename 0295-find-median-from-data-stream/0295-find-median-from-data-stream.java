93056bd4f821850169b50c0aa85dac1799119a5c
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2023-01-04 20:16:07 -0800

    Time: 1638 ms (5.01%), Space: 139.3 MB (5.16%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -0,0 +1,55 @@
+class MedianFinder {
+    List<Double> list;
+    double two = 2;
+    public MedianFinder() {
+        this.list = new ArrayList<Double>();
+    }
+    
+    public void addNum(double num) {
+       insertion(this.list, num);
+        
+    }
+    
+    public double findMedian() {
+        if (this.list.size() == 0) {
+            return 0;
+        } else if (this.list.size() == 1) {
+            return this.list.get(0);
+        } else if (this.list.size() % 2 == 0) {
+            return ((this.list.get((int)Math.floor((double)(this.list.size()-1)/ two))) + this.list.get((int)Math.ceil((double)(this.list.size()-1) / (double)2)))/ two;
+        } else {
+            return this.list.get((this.list.size()-1)/2);
+        }
+        
+    }
+    
+    private void insertion(List<Double> listToInsert, double numToInsert) {
+        int listSize = listToInsert.size();
+        boolean largestNumber = true;
+        if (listSize == 0 ) {
+            listToInsert.add(numToInsert);
+        } else {
+            for (int i = 0; i < listSize; i++) {
+                
+                if (numToInsert <= listToInsert.get(i)) {
+                    listToInsert.add(i, numToInsert);
+                    largestNumber = false;
+                    break;
+                }
+            }
+            if (largestNumber) {
+                listToInsert.add(numToInsert);
+            }
+        
+        } 
+        
+        //System.out.println(listToInsert);
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
