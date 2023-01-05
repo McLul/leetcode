2c20314ddae8685d408a9ffa9e5f0f0ea64b2fdc
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2023-01-04 20:30:51 -0800

    Time: 1551 ms (5.01%), Space: 122.2 MB (59.19%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,13 +1,14 @@
 class MedianFinder {
     List<Double> list;
     double two = 2;
+    double sizeOfList = -1;
     public MedianFinder() {
         this.list = new ArrayList<Double>();
     }
     
     public void addNum(double num) {
        insertion(this.list, num);
-        
+        this.sizeOfList += 1;
     }
     
     public double findMedian() {
@@ -16,7 +17,7 @@ class MedianFinder {
         } else if (this.list.size() == 1) {
             return this.list.get(0);
         } else if (this.list.size() % 2 == 0) {
-            return ((this.list.get((int)Math.floor((this.list.size()-1)/ two))) + this.list.get((int)Math.ceil((this.list.size()-1) / two)))/ two;
+            return ((this.list.get((int)Math.floor(sizeOfList/ two))) + this.list.get((int)Math.ceil(sizeOfList / two)))/ two;
         } else {
             return this.list.get((this.list.size()-1)/2);
         }
