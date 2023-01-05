ac0626e72b034d42f89ffcad314cd22afe467d8b
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2023-01-05 10:38:01 -0800

    Time: 587 ms (14.67%), Space: 128.2 MB (7.80%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -31,7 +31,7 @@ class MedianFinder {
         }
         int start = 0;
         int end = listToInsert.size();
-        int middle = (int)(end/2);
+        int middle = (int)Math.ceil(end/2);
         while (start < end) {
            // System.out.println(start + "start");
            // System.out.println(middle + "middle");
@@ -45,12 +45,12 @@ class MedianFinder {
             } else if (numToInsert > listToInsert.get(middle)) {
                 //System.out.println("stuck2");
                 start = middle+1;
-                middle = (int)(middle + (end-middle)/2);
+                middle = (int)Math.ceil(middle + (end-middle)/2);
 
             } else if  (numToInsert < listToInsert.get(middle)) {
                 //System.out.println("stuck3");
                 end = middle;
-                middle = (int)(end/2);
+                middle = (int)Math.ceil(end/2);
                
             }
         }
