8f8532e84eafeec74de29d5486d20f4eccc09cb8
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2023-09-05 12:09:58 -0700

    Time: 6 ms (59.68%), Space: 43 MB (32.71%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -3,6 +3,9 @@ class Solution {
         if (x < 0) {
             return false;
         }
+        if (x < 10) {
+            return true;
+        }
      ArrayList<Integer> intArray = new ArrayList<Integer>();
        while (x > 9){
            intArray.add(x%10);
