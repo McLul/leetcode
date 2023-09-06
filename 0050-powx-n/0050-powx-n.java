dcb18eb20d5593f7e4b43867fec425b83628c9c0
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2023-09-06 11:27:26 -0700

    Time: 0 ms (100.00%), Space: 41.1 MB (22.95%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,6 +1,5 @@
 class Solution {
     public double myPow(double x, int n) {
-        //int halfN = n/2; // this will deal with overflow when converting 
         if (n==0) {
             return 1;
         } else if (n==-1) {
@@ -8,7 +7,7 @@ class Solution {
         }  else if (n==1) {
             return x;
         } 
-        double half = myPow(x, n/2);
+        double half = myPow(x, n/2); // cut our calculations down by half
         double result = half * half * myPow(x, n%2);
         
         return result;
