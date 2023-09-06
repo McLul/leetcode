4a95bc456929f6ebe025d15c011f8d212a8b4f62
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2023-09-06 15:39:22 -0700

    Time: 6 ms (49.13%), Space: 41 MB (18.72%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> results = new ArrayList<String>();
+        
+        if (nums.length == 0 ) {
+            return results;
+        } 
+        int firstNum = nums[0];
+        int lastNum = nums[0];
+        if ( nums.length == 1) {
+            results.add(createRange(firstNum, lastNum));
+            return results;
+        }
+        for (int i = 1; i <nums.length;i++) { 
+            if (nums[i]-1 != lastNum) { // we found a break so add a result
+                results.add(createRange(firstNum, lastNum));
+                lastNum = nums[i];
+                firstNum = nums[i];
+            } else {
+                lastNum = nums[i]; // if no break move last ptr
+            }
+        }
+        results.add(createRange(firstNum, lastNum)); // last add needs to be done manually
+        return results;
+    }
+    private String createRange(int x, int y) { // clean up the output
+        if (x == y) {
+            return Integer.toString(x);
+        } else {
+            return Integer.toString(x)+"->"+Integer.toString(y);
+        }
+    }
+}
