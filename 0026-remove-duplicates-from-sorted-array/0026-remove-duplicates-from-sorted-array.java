7ea8cfa9cfa598792a4da1bb0f640d1955266f0a
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2023-09-06 08:21:44 -0700

    Time: 6 ms (7.92%), Space: 43.6 MB (82.76%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,12 +1,13 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         int result = 1; //result starts at one because we need to add the last unique value that wont be incorpotated into the loop
+        int lastPosition;
         for (int i = 0; i<nums.length-1;i++) {
-            int lastPosition = i+1; // we are checking the next item in the list so we end early  and only check the next
+            lastPosition = i+1; // we are checking the next item in the list so we end early  and only check the next
             while (nums[lastPosition]==nums[i] && lastPosition < nums.length-1) { //if we have reached the last value and it is the same as the previous value do nothing
                 lastPosition++;
             }
-            if (lastPosition == nums.length-1 && nums[lastPosition] == nums[i]) { //last value is the same as i so we break the loop no more unqiues
+            if (lastPosition == nums.length-1 && nums[lastPosition] == nums[i]) { //last value is the same as i so we break the loop no more uniques
                 break;
             } else if (lastPosition > i+1) { //a duplicate has been detected and we will overwrite it with next value to satisfy the inplace requirement
                 for(int ii = i+1; ii < lastPosition; ii++) {
