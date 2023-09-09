e228727f7aa9d59ffc36fd07224dd9f4ba03a4dc
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2023-09-08 19:59:04 -0700

    Time: 0 ms (100.00%), Space: 43.2 MB (65.84%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -2,16 +2,16 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int startIndex = 0;
         int endIndex = nums.length-1;
-        while (startIndex != endIndex) {
+        while (startIndex != endIndex) { //binary search to satisfy log n req
             if (target > (nums[(endIndex-startIndex)/2 + startIndex])) {
                 startIndex = (endIndex-startIndex)/2+startIndex+1; // we add +1 so we can move them closer
             } else if (target == nums[(endIndex-startIndex)/2 + startIndex]) { 
-                return (endIndex-startIndex)/2+ startIndex;
+                return (endIndex-startIndex)/2+ startIndex; // found a match stop looking
             } else {
                 endIndex = (endIndex-startIndex)/2+startIndex;
             }
         }
-        if (nums[endIndex] < target && endIndex == nums.length-1) {
+        if (nums[endIndex] < target && endIndex == nums.length-1) { // end case where it needs to be added to list
             return endIndex+1;
         }
         return endIndex;
