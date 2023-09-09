fa13f9c5d6c7955aa2dfde87ad4b13f5b5d5993c
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2023-09-09 13:36:50 -0700

    Time: 0 ms (100.00%), Space: 40.5 MB (77.60%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,18 +1,12 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         int wordLength = 0;
-        boolean word = false;
         for (int i = s.length()-1; i >= 0 ; i--) {
-            
-            if (s.charAt(i) != ' '){ // case : letters
+            if (s.charAt(i) != ' '){
                 wordLength++;
-                word = true;
-            } else if (word == true){
+            } else if (wordLength > 0){ // only returns when  we've found something 
                 return wordLength;
             }
-            
-            
-            
         }
         return wordLength;
     }
