fc677eca5148c1a6a163e4eea18f386a21c1c234
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2023-10-07 06:43:25 -0700

    Time: 35 ms (23.77%), Space: 43.7 MB (99.31%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int jump(int[] nums) {
+        
+        if (nums.length == 1) {
+            return 0;
+        } else if (nums.length == 2 || nums[0] >= nums.length-1) {
+            return 1;
+        }
+        int jumps = 1;
+        int currentPosition = 0;
+        int[] largestNum = new int[2];
+        largestNum[0] = 0;
+        largestNum[1] = nums[0];
+        while (currentPosition<nums.length) {
+            jumps++;
+            System.out.println(largestNum[0]);
+            System.out.println(largestNum[1]);
+            System.out.println(currentPosition);
+            largestNum = findLargestNumber(currentPosition+1,largestNum[1], nums); 
+            currentPosition = largestNum[0];
+            if (largestNum[1] == 0 || currentPosition + largestNum[1] >= nums.length-1 ) {
+                return jumps;
+            }
+           
+             
+        }
+        return jumps;
+    }
+    private int[] findLargestNumber(int start, int range, int[] nums) {
+        int[] result = new int[2];
+        int largestNumber = 0;
+        for (int i = start; i < start + range && i < nums.length; i++) {
+            if (nums[i]+i > largestNumber) {
+                largestNumber = nums[i]+i;
+                result[1] = nums[i];
+                result[0] = i;
+            }
+        }
+        //System.out.println(result[0]);
+        //System.out.println(result[1]);
+        return result;
+    }
+}
