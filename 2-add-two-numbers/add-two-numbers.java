6f701155d52132cbe46431339fe12a7abdae0b09
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2025-12-31 05:10:18 -0800

    Time: 1 ms (99.86%) | Memory: 46.8 MB (7.22%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -13,30 +13,23 @@ class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int l1Size = 0;
         int l2Size = 0;
-        int sizeDifference = 0;
-        ListNode endNodel1 = null;
-        ListNode endNodel2 = null;
+        ListNode secondLastNode = null;
         ListNode tempNodeHolder = l1;
         ListNode tempNodeNextHolder = null;
         ListNode resultList = new ListNode();
         ListNode largestList = null;
         ListNode smallestList = null;
-        ListNode smallestEndNode = null;
         int remainder = 0;
         int totalValNodes = 0;
         while (tempNodeHolder.next != null) {
             l1Size +=1;
             tempNodeHolder = tempNodeHolder.next;
         }
-        endNodel1 = tempNodeHolder;
         tempNodeHolder = l2;
         while (tempNodeHolder.next != null) {
             l2Size +=1;
             tempNodeHolder = tempNodeHolder.next;
         }
-        endNodel2 = tempNodeHolder;
-        smallestEndNode = l1Size <= l2Size ? endNodel1 : endNodel2;
-        //System.out.println(smallestEndNode.val);
         if (l1Size == l2Size) {
             largestList = l1;
             smallestList= l2;
@@ -44,19 +37,10 @@ class Solution {
             largestList = l1Size > l2Size ? l1 : l2;
             smallestList = l1Size < l2Size ? l1 : l2;
         }
-        sizeDifference = Math.abs(l1Size - l2Size);
-        tempNodeHolder = null;
-        while(sizeDifference > 0) {
-            smallestEndNode.next = new ListNode(0);
-            smallestEndNode = smallestEndNode.next;
-            sizeDifference -=1;
-        }
-        tempNodeHolder = smallestList;
-        //while (tempNodeHolder != null) {System.out.println(tempNodeHolder.val); tempNodeHolder = tempNodeHolder.next;}
         tempNodeHolder = resultList;
-        while (smallestList != null || remainder != 0){
-            if (smallestList != null) {
-                totalValNodes = smallestList.val + largestList.val + remainder;
+        while (largestList != null){
+            if (smallestList != null ){
+                    totalValNodes = smallestList.val + largestList.val + remainder;
                 if (totalValNodes > 9) {
                     totalValNodes -= 10;
                     remainder = 1;
@@ -65,13 +49,30 @@ class Solution {
                 }
                 smallestList = smallestList.next;
                 largestList = largestList.next;
-            } else {
-                totalValNodes = remainder;
-                remainder = 0;
+                tempNodeHolder.val = totalValNodes;
+                tempNodeHolder.next = new ListNode();
+                secondLastNode = tempNodeHolder;
+                tempNodeHolder = tempNodeHolder.next;
+            } else if (largestList != null) {
+                    totalValNodes = largestList.val + remainder;
+                if (totalValNodes > 9) {
+                    totalValNodes -= 10;
+                    remainder = 1;
+                } else {
+                    remainder = 0;
+                }
+                largestList = largestList.next;
+                tempNodeHolder.val = totalValNodes;
+                tempNodeHolder.next = new ListNode();
+                secondLastNode = tempNodeHolder;
+                tempNodeHolder = tempNodeHolder.next;
             }
-            tempNodeHolder.val = totalValNodes;
-            tempNodeHolder.next =  smallestList != null || remainder > 0 ? new ListNode() : null;
-            tempNodeHolder = tempNodeHolder.next;
+        }
+        if (remainder == 1) {
+            secondLastNode.next.val = 1;
+        }
+        else {
+            secondLastNode.next = null;
         }
         return resultList;
     }
