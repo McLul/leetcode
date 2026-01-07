2bac8454e754d307bbb1088cd0bfb2a7d2b91012
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2026-01-07 06:52:19 -0800

    Time: 6 ms (27.23%) | Memory: 46.1 MB (34.29%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        boolean result = true;
+        String stringNumber = String.valueOf(x);
+        int stringLength = stringNumber.length();
+        for (int i = 0; i< stringLength /2; i++){
+            if (stringNumber.charAt(i) != stringNumber.charAt(stringLength -1 - i)) {
+                return false;
+            }
+        }
+        return result;
+    }
+}
