bb6c3f3e0a339d91193c7f6cf7dd07bb7278f111
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2026-01-09 08:04:49 -0800

    Time: 14 ms (1.65%) | Memory: 44.6 MB (6.59%) - LeetSync

---
+++ 805-escape-the-ghosts/escape-the-ghosts.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean escapeGhosts(int[][] ghosts, int[] target) {
+        int[] playerPosition = new int[]{0,0};
+        int ghostCount = ghosts.length;
+        int playerMinMovesTarget = Math.abs(target[0]) + Math.abs(target[1]);
+        Integer ghostMinMovesTarget = null;
+        int ghostCurrMovesTarget = 0;
+        
+
+        for(int i = 0; i<ghostCount;i++) {
+            ghostCurrMovesTarget = 0;
+            if(ghosts[i][0] < 0 && target[0] < 0) {
+                ghostCurrMovesTarget += ghosts[i][0] >= target[0] ? -1*target[0] - -1*ghosts[i][0] :  -1*ghosts[i][0] - -1*target[0];
+            } else if(ghosts[i][0] < 0) {
+                ghostCurrMovesTarget += -1*ghosts[i][0] + target[0];
+            } else if( target[0] < 0) {
+                ghostCurrMovesTarget += ghosts[i][0] + -1* target[0];
+            } else {
+                ghostCurrMovesTarget += ghosts[i][0] >= target[0] ? ghosts[i][0] - target[0] : target[0] - ghosts[i][0];
+            }
+            if(ghosts[i][1] < 0 && target[1] < 0) {
+                ghostCurrMovesTarget += ghosts[i][1] >= target[1] ? -1*target[1] - -1*ghosts[i][1] :  -1*ghosts[i][1] - -1*target[1];
+            } else if(ghosts[i][1] < 0) {
+                ghostCurrMovesTarget += -1*ghosts[i][1] + target[1];
+            } else if( target[1] < 0) {
+                ghostCurrMovesTarget += ghosts[i][1] + -1* target[1];
+            } else {
+                ghostCurrMovesTarget += ghosts[i][1] >= target[1] ? ghosts[i][1] - target[1] : target[1] - ghosts[i][1];
+            }
+            System.out.println(ghostCurrMovesTarget);
+            if (ghostMinMovesTarget == null || ghostMinMovesTarget > ghostCurrMovesTarget){
+                ghostMinMovesTarget = ghostCurrMovesTarget;
+            }
+
+        }
+        System.out.println(playerMinMovesTarget);
+        System.out.println(ghostMinMovesTarget);
+        return playerMinMovesTarget < ghostMinMovesTarget ? true : false;
+
+    }
+}
