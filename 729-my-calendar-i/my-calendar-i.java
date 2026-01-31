7cb337a36e5d0195b34f2ff7cda28dd0e8c994e1
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2026-01-31 08:57:00 -0800

    Time: 47 ms (39.74%) | Memory: 47.3 MB (72.21%) - LeetSync

---
+++ 729-my-calendar-i/my-calendar-i.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+class MyCalendar {
+    ArrayList<Integer[]> calendar;
+    public MyCalendar() {
+        this.calendar = new ArrayList<Integer[]>();
+    }
+    
+    public boolean book(int startTime, int endTime) {
+        for (int i = 0; i <this.calendar.size(); i++) {
+            Integer[] booking = this.calendar.get(i);
+            // if start doesnt clear booking return false
+            if(startTime >= booking[0] && startTime < booking[1]) {
+                return false;
+            } else if (endTime > booking[0] && endTime <= booking[1]) {
+                return false;
+            } else if (startTime <= booking[0] && endTime >= booking[1]) {
+                return false;
+            }
+            
+        }
+        // if nothing clashes return true
+        Integer[] result = {startTime, endTime};
+        this.calendar.add(result);
+        return true;
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(startTime,endTime);
+ */
