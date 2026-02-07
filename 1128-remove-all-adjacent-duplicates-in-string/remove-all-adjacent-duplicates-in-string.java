5e58c227275c78e85ba3697da6e7179dcece03f3
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2026-02-07 10:47:09 -0800

    Time: 129 ms (6.70%) | Memory: 49.4 MB (5.07%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -1,82 +1,27 @@
-
-class LinkedNode {
-    char val;
-    LinkedNode next;
-    LinkedNode previous;
-    LinkedNode (char val) {
-        this.val = val;
-
-    }
-}
-class DblLinkedList {
-    LinkedNode head;
-    LinkedNode tail;
-    int count;
-    DblLinkedList () {
-        this.head = null;
-        this.tail = null;
-        this.count = 0;
-    }
-    public void addNode (char val) {
-        if (this.count == 0) {
-            this.head = new LinkedNode(val);
-            this.tail = this.head;
-        } else {
-            this.tail.next = new LinkedNode(val);
-            this.tail.next.previous = this.tail;
-            this.tail = this.tail.next;
-        }
-        this.count +=1;
-    }
-    public int size() {
-        return this.count;
-    }
-    public void removeNode () {
-        //typically we would define this however since we want direct object manipulation we will perform this directly in the loop to remove duplicates
-    }
-    
-    }
+import java.util.Stack;
+import java.util.Iterator;
 class Solution {
-    DblLinkedList linkedResult;
     String result;
     public String removeDuplicates(String s) {
-        LinkedNode currNode = null;
+        Stack<Character> resultStack = new Stack<>();
+        Iterator resultIterator;
         this.result = new String();
+        Character previousChar = null;
         // edge case length 1;
         if (s.length() < 1) {
             return s;
         }
-        // linked list for O(n) updating
-        this.linkedResult = new DblLinkedList();
         for (int i = 0 ; i < s.length(); i++) {
-            linkedResult.addNode(s.charAt(i));
-        }
-        currNode = linkedResult.head;
-        for (int i = 0 ; i < linkedResult.size()-1; i++) {
-            // edge case duplicate is on first
-            if (i == 0 && currNode.val == currNode.next.val){
-                //System.out.println(currNode.val + ", " + currNode.next.val + i);
-                currNode = currNode.next.next;
-                linkedResult.head = currNode;
-                linkedResult.count -= 2;
-                i -= 1;
-            } else if (currNode.val == currNode.next.val) {
-                //System.out.println(currNode.val + ", " + currNode.next.val + i);
-                currNode = currNode.previous;
-                if (currNode.next.next.next != null) {
-                    currNode.next.next.next.previous = currNode;
-                }
-                currNode.next = currNode.next.next.next;
-                linkedResult.count -= 2;
-                i -= 2;
-            } else {
-                currNode = currNode.next;
+            resultStack.push(s.charAt(i));
+            if (resultStack.peek().equals(previousChar)) {
+                resultStack.pop();
+                resultStack.pop();
             }
+            previousChar = resultStack.size() > 0 ? resultStack.peek() : null;
         }
-        currNode = linkedResult.head;
-        for (int i = 0; i < linkedResult.size(); i++) {
-            this.result += currNode.val;
-            currNode = currNode.next;
+        resultIterator = resultStack.iterator();
+        while (resultIterator.hasNext()) {
+            result += resultIterator.next();
         }
         return result;
     }
