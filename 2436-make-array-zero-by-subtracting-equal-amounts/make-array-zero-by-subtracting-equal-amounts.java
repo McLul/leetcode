aa894f1b52df7b02a89093609d43820d3bc3046b
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2026-02-08 14:08:05 -0800

    Time: 5 ms (13.14%) | Memory: 43.2 MB (34.75%) - LeetSync

---
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
@@ -0,0 +1,40 @@
+class Solution {
+    int result;
+    public int minimumOperations(int[] nums) {
+        this.result = 0;
+        Integer minNumber = null;
+        Integer largestNumber = null;
+        // find largest number
+        for (int i = 0; i < nums.length; i++) {
+            if (largestNumber == null || largestNumber < nums[i]) {
+                largestNumber = nums[i];
+            }
+        }
+        // find smallest number
+        while (!largestNumber.equals(minNumber)){
+            minNumber = null;
+            largestNumber = null;
+            for (int i = 0; i < nums.length; i++) {
+                if ((minNumber == null || minNumber > nums[i]) && nums[i] > 0) {
+                    minNumber = nums[i];
+                }
+                if (largestNumber == null || largestNumber < nums[i]) {
+                    largestNumber = nums[i];
+                }
+                
+            }
+            if (minNumber == null) {
+                return this.result;
+            } else {
+                for (int i = 0; i < nums.length; i++) {
+                    nums[i] -= minNumber;
+                }
+            }
+            this.result += 1;
+            //if (largestNumber == 0) {return this.result;}
+            // find minimum number of factors per.
+        }
+        return this.result;
+    }
+    
+}
