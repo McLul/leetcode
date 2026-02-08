ca97c50406fab98826a17a28708c5308e4662d20
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2026-02-08 14:30:22 -0800

    Time: 3 ms (25.17%) | Memory: 42.5 MB (99.49%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
@@ -2,24 +2,25 @@ class Solution {
     int result;
     public int minimumOperations(int[] nums) {
         this.result = 0;
-        Integer minNumber = null;
-        Integer maxNumber = 0;
+        int minNumber = 0;
+        int maxNumber = 1;
         int checkedNumber = 0;
         // find smallest number
-        while (!maxNumber.equals(minNumber)){
-            minNumber = null;
-            maxNumber = null;
+        while (maxNumber != minNumber){
+            minNumber = 0;
+            maxNumber = 0;
             for (int i = 0; i < nums.length; i++) {
                 checkedNumber = nums[i];
-                if ((minNumber == null || minNumber > checkedNumber) && checkedNumber > 0) {
+                if ( minNumber == 0 || (minNumber > checkedNumber && checkedNumber > 0)) {
                     minNumber = checkedNumber;
                 }
-                if (maxNumber == null || maxNumber < checkedNumber) {
+                if ( maxNumber < checkedNumber) {
                     maxNumber = checkedNumber;
                 }
                 
             }
-            if (minNumber == null) {
+            
+            if (maxNumber <= 0) {
                 return this.result;
             } else {
                 for (int i = 0; i < nums.length; i++) {
