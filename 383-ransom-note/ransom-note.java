03df36738ab46377cbaa5935b9e659a7a934b023
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2026-02-23 07:31:14 -0800

    Time: 3 ms (55.30%) | Memory: 46.5 MB (70.83%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -8,19 +8,22 @@ class Solution {
             return this.result;
         }
         Character charTested = null;
+        Integer intTested = null;
         HashMap<Character, Integer> stringMap = new HashMap<>();
         for (int i = 0; i < ransomNote.length(); i++) {
             charTested = ransomNote.charAt(i);
-            if (stringMap.get(charTested) == null) {
+            intTested = stringMap.get(charTested);
+            if (intTested == null) {
                 stringMap.put(charTested,1);
             } else {
-                stringMap.put(charTested,stringMap.get(charTested) + 1);
+                stringMap.put(charTested,intTested + 1);
             }
         }
         for (int i = 0 ; i < magazine.length(); i++) {
             charTested = magazine.charAt(i);
-            if (stringMap.get(charTested) == null) { // do nothing
-            } else if (stringMap.get(charTested).equals(1)) {
+            intTested = stringMap.get(charTested);
+            if (intTested == null) { // do nothing
+            } else if (intTested.equals(1)) {
                 stringMap.put(charTested,null);
                 letterCount -= 1;
                 if (letterCount == 0) {
@@ -28,7 +31,7 @@ class Solution {
                     return this.result;
                 }
             } else {
-                stringMap.put(charTested,stringMap.get(charTested) - 1);
+                stringMap.put(charTested,intTested - 1);
                 letterCount -= 1;
                 if (letterCount == 0) {
                     this.result = true;
