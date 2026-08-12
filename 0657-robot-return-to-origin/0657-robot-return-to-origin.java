250d18bcd22696749f0844f425015c22b30ca388
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2026-08-12 07:25:37 -0700

    Time: 6 ms (51.88%), Space: 45.5 MB (77.99%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,6 +1,6 @@
 import java.util.HashMap;
 
-/* simplest solution
+// simplest solution
 class Solution {
     public boolean judgeCircle(String moves) {
         
@@ -8,11 +8,11 @@ class Solution {
         int posY = 0;
         for (int i = 0; i < moves.length(); i++) {
             switch (moves.charAt(i)) {
-                case 'U':
-                    posY++;
-                    break; 
                 case 'D':
                     posY--;
+                    break; 
+                case 'U':
+                    posY++;
                     break;
                 case 'R':
                     posX++;
@@ -25,20 +25,5 @@ class Solution {
         return (posX == 0 && posY == 0) ? true : false;
     }
 }
-*/
 
-class Solution {
-    public boolean judgeCircle(String moves) {
-        HashMap<Character, Integer> moveSet = new HashMap<Character, Integer>();
-        Character testChar = 'S';
-        moveSet.put('U',0);
-        moveSet.put('D',0);
-        moveSet.put('L',0);
-        moveSet.put('R',0);
-        for (int i = 0; i < moves.length(); i++) {
-            testChar = moves.charAt(i);
-            moveSet.put(testChar,moveSet.get(testChar)+1);
-        }
-    return (Integer.sum(moveSet.get('U'),-1*moveSet.get('D')) == 0 && Integer.sum(moveSet.get('L'),-1*moveSet.get('R')) == 0) ? true : false;
-    }
-}
+
