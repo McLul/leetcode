815c78985ed7daeb1ec22c880be37b8f4fddd4b8
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2026-08-12 07:04:09 -0700

    Time: 6 ms (51.88%), Space: 45.6 MB (58.84%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int posX = 0;
+        int posY = 0;
+        for (int i = 0; i < moves.length(); i++) {
+            switch (moves.charAt(i)) {
+                case 'U':
+                    posY++;
+                    break; 
+                case 'D':
+                    posY--;
+                    break;
+                case 'R':
+                    posX++;
+                    break;
+                case 'L':
+                    posX--;
+                    break;
+            }
+        }
+        return (posX == 0 && posY == 0) ? true : false;
+    }
+}
