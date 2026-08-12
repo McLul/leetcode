19f09db5b92f697cafa10186fd490697be7d6261
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2026-08-12 07:26:18 -0700

    Time: 5 ms (68.97%), Space: 45.1 MB (98.31%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -8,15 +8,15 @@ class Solution {
         int posY = 0;
         for (int i = 0; i < moves.length(); i++) {
             switch (moves.charAt(i)) {
-                case 'D':
-                    posY--;
-                    break; 
                 case 'U':
                     posY++;
                     break;
                 case 'R':
                     posX++;
                     break;
+                case 'D':
+                    posY--;
+                    break; 
                 case 'L':
                     posX--;
                     break;
