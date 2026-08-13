96d6743f3d9a2b5a8a80f8ff885231b673ebfd72
Frederick V Pascua <104282669+McLul@users.noreply.github.com>
2026-08-13 08:36:43 -0700

    Time: 1 ms (99.29%), Space: 46.9 MB (71.07%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean areSimilar(int[][] mat, int k) {
         for (int i = 0; i < mat.length; i++) {
-            for (int ii = 0; ii < mat[i].length; ii++) {
-                if (mat[i][ii] != mat[i][(ii + k) % mat[i].length]) {
+            for (int ii = 0; ii < mat[i].length; ii++) { // no need to check for even or odd because we are only checking if they are identical so + or - wont matter it will only flip starting and ending position [eg leftshift pos(3-2) = pos(1) is the same as rightshift pos(1+2) = pos(3)]
+                if (mat[i][ii] != mat[i][(ii + k) % mat[i].length]) { // we use modulus to reloop the the row and check all places vs their final positions
                     return false;
                 }
             }
